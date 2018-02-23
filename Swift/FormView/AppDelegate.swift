7937f83486f7911edcb41ae64382af9c0d68cdbf
huangboju <bhuang@xiaohongshu.com>
2018-02-23 18:05:52 +0800

    修改国家

--- Swift/FormView/AppDelegate.swift
+++ Swift/FormView/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let strs = [
-            "朝鲜+850"
+            "厄瓜多尔+593",
+            "俄罗斯+7"
         ]
         
         strs.forEach(convert)
