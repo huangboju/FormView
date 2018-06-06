9eeb41fff7e6acd0e5cc22c971a4e0675b01db5a
huangboju <bhuang@xiaohongshu.com>
2018-06-06 16:45:47 +0800

    删除没用代码

--- Objc/FormView/AppDelegate.m
+++ Objc/FormView/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 
-#import <CommonCrypto/CommonDigest.h>
+#import "XYEncyptionUtil.h"
 
 @interface AppDelegate ()
 
@@ -19,123 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-//    NSMutableArray *results1 = [NSMutableArray array];
-//    NSMutableArray *results2 = [NSMutableArray array];
-//    NSMutableArray *results3 = [NSMutableArray array];
-//    NSMutableArray *results4 = [NSMutableArray array];
-//
-//    NSRange range1 = NSMakeRange(1, 10);
-//    NSRange range2 = NSMakeRange(21, 10);
-//    NSRange range3 = NSMakeRange(41, 10);
-//    NSRange range4 = NSMakeRange(61, 10);
-//
-//    for (int i = 0; i < 100000; i++) {
-//        NSString *md5Str = [self strToMD5WithStr:[NSString stringWithFormat:@"%d", i]];
-//        int result = [self calculateHexMod:md5Str] + 1;
-//        if (NSLocationInRange(result, range1)) {
-//            [results1 addObject:[NSNumber numberWithInt:result]];
-//        } else if (NSLocationInRange(result, range2)) {
-//            [results2 addObject:[NSNumber numberWithInt:result]];
-//        } else if (NSLocationInRange(result, range3)) {
-//            [results3 addObject:[NSNumber numberWithInt:result]];
-//        } else if (NSLocationInRange(result, range4)) {
-//            [results4 addObject:[NSNumber numberWithInt:result]];
-//        }
-//    }
-//    NSLog(@"%zd", results1.count);
-//    NSLog(@"%zd", results2.count);
-//    NSLog(@"%zd", results3.count);
-//    NSLog(@"%zd", results4.count);
     return YES;
-}
-
-- (NSString *)strToMD5WithStr:(NSString *)str {
-    //转化为UTF8格式字符串
-    const char *ptr = [str UTF8String];
-    
-    //开辟一个16字节数组
-    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
-    
-    //调用官方封装的加密方法, 将ptr开始的字符串存储到md5Buffer[]中
-    CC_MD5(ptr, strlen(ptr), md5Buffer);
-    
-    //转换位NSString并返回
-    NSMutableString *output = [NSMutableString stringWithCapacity: CC_MD5_DIGEST_LENGTH * 2];
-    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i ++) {
-        [output appendFormat: @"%02x", md5Buffer[i]];
-    }
-    return output;
-}
-
-- (int)calculateHexMod:(NSString *)hexString {
-    static NSArray <NSNumber *>*targets;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        targets = @[
-                    @76,
-                    @16,
-                    @56,
-                    @96,
-                    @36
-                    ];
-    });
-    
-    int result = 0;
-    NSInteger strLength = hexString.length;
-    for (int i = 0; i < strLength; i++) {
-        NSString *subStr = [hexString substringWithRange:NSMakeRange(strLength - 1 - i, 1)];
-        int number = (int)strtoull([subStr UTF8String], NULL, 16);
-        if (i == 0) {
-            result = number;
-            continue;
-        }
-        int idx = i % 5;
-        int n = targets[idx].intValue;
-        result += (number * n);
-    }
-
-    result %= 100;
-
-    return result;
-}
-
-- (void)loadAttachmentForUrlString:(NSString *)urlStr
-                          withType:(NSString *)type {
-
-    NSURL *attachmentURL = [NSURL URLWithString:urlStr];
-    NSString *fileExt = [self fileExtensionForMediaType:type];
-    
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    [[session downloadTaskWithURL:attachmentURL
-                completionHandler:^(NSURL *temporaryFileLocation, NSURLResponse *response, NSError *error) {
-                    if (error != nil) {
-                        NSLog(@"%@", error.localizedDescription);
-                    } else {
-                        NSFileManager *fileManager = [NSFileManager defaultManager];
-                        NSURL *localURL = [NSURL fileURLWithPath:[temporaryFileLocation.path stringByAppendingString:fileExt]];
-                        [fileManager moveItemAtURL:temporaryFileLocation toURL:localURL error:&error];
-                        NSError *attachmentError = nil;
-
-                        if (attachmentError) {
-                            NSLog(@"%@", attachmentError.localizedDescription);
-                        }
-                    }
-                }] resume];
-    
-}
-
-- (NSString *)fileExtensionForMediaType:(NSString *)type {
-    NSString *ext = type;
-    if ([type isEqualToString:@"image"]) {
-        ext = @"jpg";
-    }
-    if ([type isEqualToString:@"video"]) {
-        ext = @"mp4";
-    }
-    if ([type isEqualToString:@"audio"]) {
-        ext = @"mp3";
-    }
-    return [@"." stringByAppendingString:ext];
 }
 
 
