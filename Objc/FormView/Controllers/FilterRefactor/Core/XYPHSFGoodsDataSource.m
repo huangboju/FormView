7d81705405c9e9626e99fd09f866218c3811e6f9
huangboju <bhuang@xiaohongshu.com>
2019-06-12 14:45:36 +0800

    Update XYPHSFGoodsDataSource.m

--- Objc/FormView/Controllers/FilterRefactor/Core/XYPHSFGoodsDataSource.m
+++ Objc/FormView/Controllers/FilterRefactor/Core/XYPHSFGoodsDataSource.m
@@ -20,11 +20,11 @@
     if (self.isLoading) { return; }
     self.isLoading = YES;
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:@"filter" ofType:@"json"];
+        NSData *data = [NSData dataWithContentsOfFile:filePath];
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
         dispatch_async(dispatch_get_main_queue(), ^{
             self.isLoading = NO;
-            NSString *filePath = [[NSBundle mainBundle] pathForResource:@"filter" ofType:@"json"];
-            NSData *data = [NSData dataWithContentsOfFile:filePath];
-            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
             if (completion) {
                 completion([XYPHSearchGoodsFilter yy_modelWithJSON:json[@"data"]]);
             }
